f26e2f592d8b85bb2a4f774891c839a45424415a
arunNewPhoenix <2245arunkumarz@gmail.com>
2023-08-13 03:45:16 +0530

    added the matplotlib

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from flask import Flask, send_file
 from reportlab.lib.pagesizes import letter
-from reportlab.platypus import SimpleDocTemplate, Table, TableStyle
+from reportlab.platypus import SimpleDocTemplate, Table, TableStyle, Image, Spacer
 from reportlab.lib import colors
 from reportlab.lib.units import inch
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from io import BytesIO
+import matplotlib.pyplot as plt
 
 app = Flask(__name__)
 
@@ -11,26 +14,36 @@ def generate_pdf():
     # Create a PDF document with a margin of 23 pixels from all sides
     doc = SimpleDocTemplate("table_example.pdf", pagesize=letter, leftMargin=23, rightMargin=23, topMargin=23, bottomMargin=23)
 
-    # Define data for the table (2 columns and 4 rows)
+    # Define data for the table (2 columns and 5 rows)
     data = [
-        ["Row 1, Col 1", "Row 1, Col 2"],
-        ["Row 2, Col 1", "Row 2, Col 2"],
-        ["Row 3, Col 1", "Row 3, Col 2"],
-        ["Row 4, Col 1", "Row 4, Col 2"],
+        ["Cell 1"],
+        ["Cell 2"],
+        ["Cell 3"],
+        ["Cell 4"],
     ]
 
     # Calculate available width and height after considering margins
     available_width = doc.pagesize[0] - doc.leftMargin - doc.rightMargin
     available_height = doc.pagesize[1] - doc.topMargin - doc.bottomMargin
 
-    # Calculate cell width
-    cell_width = available_width / len(data[0])
-
-    # Calculate cell height to fit 3 rows within the available height
-    cell_height = (available_height - 3 * inch) / 3
+    # Calculate cell width and height
+    cell_width = (available_width - 23) / 2  # Divide by 2 for 2 columns
+    cell_height = (available_height - 3 * inch) / len(data)
 
     # Create a table with the data and adjusted cell dimensions
-    table = Table(data, colWidths=[cell_width] * len(data[0]), rowHeights=[cell_height] * len(data))
+    table_data = []
+    for i in range(len(data)):
+        fig, ax = plt.subplots()
+        ax.plot([1, 2, 3, 4], [i * 10 + 10, i * 10 + 20, i * 10 + 15, i * 10 + 5])
+        canvas = FigureCanvas(fig)
+        buffer = BytesIO()
+        canvas.print_png(buffer)
+        plt.close(fig)
+        image = Image(buffer, width=cell_width, height=cell_height)
+        empty_cell = Spacer(width=cell_width, height=cell_height)  # Empty cell
+        table_data.append([image, empty_cell])
+
+    table = Table(table_data, colWidths=[cell_width, cell_width], rowHeights=[cell_height] * len(data))
 
     # Add style to the table (black border for all cells)
     style = TableStyle([
@@ -47,4 +60,3 @@ def generate_pdf():
 
 if __name__ == '__main__':
     app.run(debug=True)
-
